432d4de2c81eb536229f3f75303a41a7f0daa2db
blue-squirrel <1440054388@qq.com>
2022-09-12 01:06:15 +0800

    吉他谱网站第一次提交

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    我的吉他谱网站
+  </WelcomeItem>
+</template>
