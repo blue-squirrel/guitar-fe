2fadc94018dba116a495539a32d206fcba15dff4
blue-squirrel <1440054388@qq.com>
2022-09-12 15:54:17 +0800

    less

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    我的吉他谱网站
-  </WelcomeItem>
+  我的吉他谱网站
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,26 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
+
+<style scoped lang="less">
+.el-container {
+  width: 100%;
+  .el-header {
+    width: 100%;
+  }
+  .el-main {
+    width: 1280px;
+  }
+}
+</style>
