4e7fb4a578e4ed3d3df16f3e13ff33996f6167c2
blue-squiirel <1440054388@qq.com>
2023-04-12 01:31:31 +0800

    21

--- vite.config.ts
+++ vite.config.ts
@@ -23,13 +23,13 @@ export default defineConfig({
     }
   },
   server: {
-    proxy: {
-      '/api': {
-        // target: 'http://127.0.0.1:7001',
-        target: 'http://180.76.228.38:7001',
-        changeOrigin: true,
-        // rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     // target: 'http://127.0.0.1:7001',
+    //     target: 'http://180.76.228.38:7001',
+    //     changeOrigin: true,
+    //     // rewrite: path => path.replace(/^\/api/, '')
+    //   }
+    // }
   }
 })
