0e58c7cef77ff8114aac3cd2dccd26572ce9a84d
blue-squiirel <1440054388@qq.com>
2023-04-17 00:12:41 +0800

    配置

---
+++ config/index.ts
@@ -0,0 +1,6 @@
+export const config = {
+  secretId: 'AKIDHNDm58vRfdKlvbCxSgdlcCWaNs6uWqtf',
+  secretKey: 'ZqqZ0E9nAF3ZogHbePt5zbt0rSmtiGWV',
+  bucketName: 'blue-squirrel-1257450790',
+  region: 'ap-beijing'
+}
--- src/util/cos.ts
+++ src/util/cos.ts
@@ -1,29 +1,18 @@
 // import moment from 'moment'
 // import { credential } from '@/api/common'
 import COS from 'cos-js-sdk-v5'
+import { config } from '../../config/index'
 export const uploadCos = async (file, filePath) => {
   // 为了保证桶的数据安全，数据桶的参数需要后端给你传，这样比较安全
-  //  const res = await credential({ bucketKey: 'gas_bucket' })
+  const { secretId, secretKey, bucketName, region } = config
   let res = {
-    tmpSecretId: 'AKIDHNDm58vRfdKlvbCxSgdlcCWaNs6uWqtf',
-    tmpSecretKey: 'ZqqZ0E9nAF3ZogHbePt5zbt0rSmtiGWV',
     sessionToken: 'xxxxx',
-    bucketName: 'blue-squirrel-1257450790',
-    region: 'ap-beijing',
     expiredTime: 1580000900
   }
-  const {
-    tmpSecretId,
-    tmpSecretKey,
-    sessionToken,
-    bucketName,
-    region,
-    expiredTime
-  } = res
   // 创建桶的实例
   const cos = new COS({
-    SecretId: tmpSecretId, // 身份识别ID
-    SecretKey: tmpSecretKey // 身份秘钥
+    SecretId: secretId, // 身份识别ID
+    SecretKey: secretKey // 身份秘钥
   })
   //  const cos = new COS({
   //    // 必选参数
